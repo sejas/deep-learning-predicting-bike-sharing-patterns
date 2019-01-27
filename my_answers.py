e5e7eaf4493dbc61e26ce9717aa8c3b589a6ab0e
Sejas <antonio@sejas.es>
2019-01-27 10:47:15 +0000

    sigmoid function for hidden layers added

--- my_answers.py
+++ my_answers.py
@@ -16,19 +16,11 @@ class NeuralNetwork(object):
                                        (self.hidden_nodes, self.output_nodes))
         self.lr = learning_rate
         
-        #### TODO: Set self.activation_function to your implemented sigmoid function ####
+        #### DONE: Set self.activation_function to your implemented sigmoid function ####
         #
         # Note: in Python, you can define a function with a lambda expression,
         # as shown below.
-        self.activation_function = lambda x : 0  # Replace 0 with your sigmoid calculation.
-        
-        ### If the lambda code above is not something you're familiar with,
-        # You can uncomment out the following three lines and put your 
-        # implementation there instead.
-        #
-        #def sigmoid(x):
-        #    return 0  # Replace 0 with your sigmoid calculation here
-        #self.activation_function = sigmoid
+        self.activation_function = lambda x : 1 / (1 + np.exp(-x)) # sigmoid calculation.
                     
 
     def train(self, features, targets):
